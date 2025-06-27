038c04d8e350775a1ff3eee69ec71d0a4f7e83d8
王强 <wangqiang@hfvast.com>
2025-06-28 00:12:31 +0800

    feat: init crepe editor

--- src/views/edit/EditView.vue
+++ src/views/edit/EditView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { MilkdownProvider } from '@milkdown/vue';
-import MilkdownEditor from "./MilkdownEditor.vue"
+import MilkdownEditorWrapper from "./MilkdownEditorWrapper.vue"
 </script>
 <template>
-  <MilkdownProvider>
-    <MilkdownEditor />
-  </MilkdownProvider>
+  <div class="w-full flex justify-center">
+    <div class="w-80 bg-amber-200"></div>
+    <div class="w-[calc(100vw-40rem)] h-screen overflow-y-auto">
+      <MilkdownEditorWrapper />
+    </div>
+    <div class="w-80 bg-amber-200">
+    </div>
+  </div>
 </template>
--- src/views/edit/MilkdownEditor.vue
+++ src/views/edit/MilkdownEditor.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
+import { Crepe } from "@milkdown/crepe";
 import { Milkdown, useEditor } from "@milkdown/vue";
-import { Editor, rootCtx, defaultValueCtx } from "@milkdown/kit/core";
-import { commonmark } from "@milkdown/kit/preset/commonmark";
-import { listener, listenerCtx } from "@milkdown/kit/plugin/listener";
 
 const markdown: string = `# Milkdown Vue Crepe
 
@@ -10,24 +8,17 @@ const markdown: string = `# Milkdown Vue Crepe
 
 This is a demo for using Crepe with **Vue**.`;
 
-useEditor((root) =>
-  Editor.make()
-    .config((ctx) => {
-      ctx.set(rootCtx, root);
-      ctx.set(defaultValueCtx, markdown);
-      // Add markdown listener for auto-save
-      ctx.get(listenerCtx).markdownUpdated((ctx, markdown) => {
-        // Save content to your backend or storage
-        saveToBackend(markdown);
-      });
-    })
-    .use(commonmark)
-    .use(listener)
-)
+useEditor((root) => {
+  const crepe = new Crepe({ root, defaultValue: markdown })
+  crepe.on(listener => {
+    listener.markdownUpdated(() => {
+      console.log("Markdown updated:", crepe.getMarkdown());
+    });
 
-const saveToBackend = (markdown: string) => {
-  console.log("markdown: ", markdown);
+  })
+  return crepe;
 }
+)
 </script>
 
 <template>
