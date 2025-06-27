8a767c35d7487e3ca9f5eec408db454f53a6402f
王强 <wangqiang@hfvast.com>
2025-06-28 00:12:24 +0800

    feat: init crepe editor

---
+++ src/views/edit/MilkdownEditorWrapper.vue
@@ -0,0 +1,12 @@
+
+
+<script setup lang="ts">
+import { MilkdownProvider } from "@milkdown/vue";
+import MilkdownEditor from "./MilkdownEditor.vue";
+
+</script>
+<template>
+  <MilkdownProvider>
+    <MilkdownEditor />
+  </MilkdownProvider>
+</template>
