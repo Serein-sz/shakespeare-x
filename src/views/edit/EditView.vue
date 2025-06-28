6c2d4024412073888a86e68643cbe661eb3b1d1b
王强 <wangqiang@hfvast.com>
2025-06-29 03:35:09 +0800

    feat: update background-color

--- src/views/edit/EditView.vue
+++ src/views/edit/EditView.vue
@@ -15,13 +15,13 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div class="w-full h-screen flex justify-center">
-    <div class="w-80 bg-amber-200"></div>
+  <div class="w-full h-screen flex justify-center bg-[#1A1A1A]">
+    <div class="w-80"></div>
     <Separator orientation="vertical" />
     <div class="w-[calc(100vw-40rem)] h-screen overflow-y-auto">
       <MilkdownEditorWrapper ref="editorWrapperRef" />
     </div>
     <Separator orientation="vertical" class="w-1" />
-    <TitleView class="w-80 h-full overflow-y-auto" />
+    <TitleView class="w-80 h-full overflow-y-auto mt-12" />
   </div>
 </template>
